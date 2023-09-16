61e0d1c2af981cdc022fb58068e3f5897d26ec9f
ivanc <ivancrak123@gmail.com>
2023-09-16 22:55:29 +0200

    Release v1.0

---
+++ readme.md
@@ -0,0 +1,113 @@
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/LaCrak27/RailwayBot">
+    <img src="https://devicons.railway.app/i/railway-light.svg" alt="Logo" width="80" height="80">
+  </a>
+
+<h1 align="center">RailwayBot</h1>
+
+  <p align="center">
+    A companion bot for railway users that offers better deployment updates as well as usage stats.
+    <br />
+    <a href="https://github.com/LaCrak27/RailwayBot/issues">Report Bug/Request features</a>
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-railwaybot">About RailwayBot</a>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#features">Features</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+  </ol>
+</details>
+
+## About RailwayBot
+
+RailwayBot is an open source companion bot for railway users, made for fulfilling the needs of knowing if you've got a memory leak, or one of your services has crashed, all from the comfort of discord.
+
+## Getting Started
+
+### Prerequisites
+
+Since this bot is made in typescript, it uses discord.js, and other packages, so make sure you have both node and npm installed.
+
+### Installation
+
+1. Clone the repo
+   ```sh
+   git clone https://github.com/LaCrak27/RailwayBot.git
+   ```
+2. Install NPM packages
+   ```sh
+   npm i
+   ```
+3. Create an application on discord's developer portal and get it's bot token. [(help)](https://discordjs.guide/preparations/setting-up-a-bot-application.html#creating-your-bot)
+4. Get a railway api token linked to your account. [(help)](https://docs.railway.app/reference/public-api#authentication)
+
+5. Prepare your enviroment file (`.env`)
+   ```env
+    DISCORDTOKEN=(discord bot token)
+    LOGCHANNEL=(channel id for deployment and crash logs)
+    USAGECHANNEL=(channel id for usage reports)
+    RAILWAYAPIKEY=(railway api key)
+   ```
+
+<p align="right">(<a href="#about-railwaybot">back to top</a>)</p>
+
+## Usage
+
+If you only intend on using the bot, then you can just clone the repository, host it on your hosting platform of choice filling all the enviroment variables (we all know what that platform is don't we), and invite the resulting bot to your server.
+
+After this is done, you need to add a webhook to railway for every project you intend on logging its crashes/deployments. Said webhook must point to "<YourBot's URL>/railway".
+
+<p align="right">(<a href="#about-railwaybot">back to top</a>)</p>
+
+## Features
+
+- Deploy logs showing all kinds of information about the deploy.
+- Crash alerts for any of your services.
+- Hourly usage reports, showing a detailed breakdown.
+
+See the [open issues](https://github.com/LaCrak27/RailwayBot/issues) for a full list of proposed features (and known issues).
+
+<p align="right">(<a href="#about-railwaybot">back to top</a>)</p>
+
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#about-railwaybot">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#about-railwaybot">back to top</a>)</p>
--- src/railwaybot.ts
+++ src/railwaybot.ts
@@ -1,12 +1,13 @@
 require("dotenv").config();
 
-import { Channel, TextChannel, Client, EmbedBuilder } from "discord.js";
-import { stat } from "fs";
+import { Channel, TextChannel, Client, EmbedBuilder, createComponent } from "discord.js";
+import { request, gql, GraphQLClient } from 'graphql-request';
 const token = process.env.DISCORDTOKEN;
 
 const express = require('express');
 const bodyParser = require('body-parser');
 var cors = require('cors');
+var cron = require('node-cron');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -29,10 +30,119 @@ client.on("ready", async () => {
     if (!client.user || !client.application) {
         return;
     }
-
+    GetUsage();
     console.log(`RailwayBot is online, logged in as ${client.user.username}#${client.user.discriminator} with id ${client.user.id}`);
 });
 
+const graphQLClient = new GraphQLClient('https://backboard.railway.app/graphql/v2', {
+    headers: {
+        "Authorization": "Bearer 86953758-7d7a-4504-9fcc-7431ecba452e"
+    },
+});
+const query = gql`
+query me {
+    usage(measurements: [CPU_USAGE, MEMORY_USAGE_GB, NETWORK_TX_GB])
+    {
+      value
+      measurement
+      tags
+      {
+        projectId
+      }
+    }
+    estimatedUsage(measurements: [CPU_USAGE, MEMORY_USAGE_GB, NETWORK_TX_GB])
+    {
+      estimatedValue
+      measurement
+      projectId
+    }
+    me {
+      projects {
+        edges {
+          node {
+            id
+            name
+          }
+        }
+      }
+    }
+  }
+`;
+
+//Cron-job for usage alerts
+cron.schedule('0 * * * *', async () => {
+    GetUsage();
+});
+async function GetUsage() {
+    const results = await graphQLClient.request(query) as GraphQLResponse;
+    let projects: Project[] = [];
+    for (let i = 0; i < results.me.projects.edges.length; i++) {
+        if (results.usage.filter(usage => usage.measurement === 'CPU_USAGE' && usage.tags.projectId === results.me.projects.edges[i].node.id).length != 0 && results.usage.filter(usage => usage.measurement === 'MEMORY_USAGE_GB' && usage.tags.projectId === results.me.projects.edges[i].node.id).length != 0 && results.usage.filter(usage => usage.measurement === 'NETWORK_TX_GB' && usage.tags.projectId === results.me.projects.edges[i].node.id).length != 0) {
+            var project: Project = {
+                name: results.me.projects.edges[i].node.name,
+                id: results.me.projects.edges[i].node.id,
+                CpuUsage: results.usage.filter(usage => usage.measurement === 'CPU_USAGE' && usage.tags.projectId === results.me.projects.edges[i].node.id)[0].value,
+                MemUsage: results.usage.filter(usage => usage.measurement === 'MEMORY_USAGE_GB' && usage.tags.projectId === results.me.projects.edges[i].node.id)[0].value,
+                Egress: results.usage.filter(usage => usage.measurement === 'NETWORK_TX_GB' && usage.tags.projectId === results.me.projects.edges[i].node.id)[0].value,
+                EstimatedCpuUsage: results.estimatedUsage.filter(usage => usage.measurement === 'CPU_USAGE' && usage.projectId === results.me.projects.edges[i].node.id)[0].estimatedValue,
+                EstimatedMemUsage: results.estimatedUsage.filter(usage => usage.measurement === 'MEMORY_USAGE_GB' && usage.projectId === results.me.projects.edges[i].node.id)[0].estimatedValue,
+                EstimatedEgress: results.estimatedUsage.filter(usage => usage.measurement === 'NETWORK_TX_GB' && usage.projectId === results.me.projects.edges[i].node.id)[0].estimatedValue,
+            };
+            projects.push(project);
+        }
+    }
+    const usageChannel = await client.channels.fetch(`${process.env.USAGECHANNEL}`) as TextChannel
+    usageChannel.send({ content: "**USAGE REPORT**" })
+    for (let i = 0; i < projects.length; i++) {
+        const project = projects[i];
+        const embed = new EmbedBuilder()
+            .setAuthor({
+                name: "Usage Metrics",
+            })
+            .setTitle(project.name)
+            .setDescription(`**TOTAL COST**: $${(project.CpuUsage * 0.000463 + project.MemUsage * 0.000231 + project.Egress * 0.1).toFixed(4)}\n**TOTAL ESTIMATED COST**: $${(project.EstimatedCpuUsage * 0.000463 + project.EstimatedMemUsage * 0.000231 + project.EstimatedEgress * 0.1).toFixed(4)}\nBreakdown below:`)
+            .addFields(
+                {
+                    name: "CPU Usage",
+                    value: `${project.CpuUsage.toFixed(4)}vCores/min\n($${(project.CpuUsage * 0.000463).toFixed(4)})`,
+                    inline: true
+                },
+                {
+                    name: "Memory Usage",
+                    value: `${project.MemUsage.toFixed(4)}GB/min\n($${(project.MemUsage * 0.000231).toFixed(4)})`,
+                    inline: true
+                },
+                {
+                    name: "Egress",
+                    value: `${project.Egress.toFixed(4)}GB/min\n($${(project.Egress * 0.1).toFixed(4)})`,
+                    inline: true
+                },
+                {
+                    name: "Estimated CPU Usage",
+                    value: `${project.EstimatedCpuUsage.toFixed(4)}vCores/min\n($${(project.EstimatedCpuUsage * 0.000463).toFixed(4)})`,
+                    inline: true
+                },
+                {
+                    name: "Estimated Memory Usage",
+                    value: `${project.EstimatedMemUsage.toFixed(4)}GB/min\n($${(project.EstimatedMemUsage * 0.000231).toFixed(4)})`,
+                    inline: true
+                },
+                {
+                    name: "Estimated Egress",
+                    value: `${project.EstimatedEgress.toFixed(4)}GB/min\n($${(project.EstimatedEgress * 0.1).toFixed(4)})`,
+                    inline: true
+                },
+            )
+            .setColor("#00b0f4")
+            .setFooter({
+                text: "Railway Usage Metrics",
+                iconURL: "https://devicons.railway.app/i/railway-dark.svg",
+            })
+            .setTimestamp();
+        usageChannel.send({ embeds: [embed] })
+    }
+}
+//Webhook Setup
 app.post('/railway', (req: any, res: any) => {
     processWebhook(req.body);
     console.log(req.body);
@@ -68,78 +178,76 @@ function processWebhook(body: WebhookBody) {
 async function SendDeploymentMessage(projectName: string, environmentName: string, serviceName: string, deploymentCommit: string, deploymentCommitAuthor: string, status: string) {
     const channel = await client.channels.fetch(`${process.env.LOGCHANNEL}`) as TextChannel
     let embed: EmbedBuilder;
-    if(status != "CRASHED")
-    {
-    embed = new EmbedBuilder()
-        .setAuthor({
-            name: "Deployment info:",
-        })
-        .setTitle(projectName)
-        .setDescription(`**${status}**`)
-        .addFields(
-            {
-                name: "Enviroment",
-                value: environmentName,
-                inline: true
-            },
-            {
-                name: "Service",
-                value: serviceName,
-                inline: true
-            },
-            {
-                name: "Commit Author",
-                value: deploymentCommitAuthor,
-                inline: true
-            },
-            {
-                name: "Commit Message",
-                value: deploymentCommit,
-                inline: false
-            },
-        )
-        .setColor("#00ff40")
-        .setFooter({
-            text: "RailwayBot",
-            iconURL: "https://devicons.railway.app/i/railway-light.svg",
-        })
-        .setTimestamp();
-    }
-    else
-    {
+    if (status != "CRASHED") {
         embed = new EmbedBuilder()
-        .setAuthor({
-            name: "Deployment Crashed:",
-        })
-        .setTitle(projectName)
-        .addFields(
-            {
-                name: "Enviroment",
-                value: environmentName,
-                inline: true
-            },
-            {
-                name: "Service",
-                value: serviceName,
-                inline: true
-            },
-            {
-                name: "Commit Author",
-                value: deploymentCommitAuthor,
-                inline: true
-            },
-            {
-                name: "Commit Message",
-                value: deploymentCommit,
-                inline: false
-            },
-        )
-        .setColor("#ff0000")
-        .setFooter({
-            text: "RailwayBot",
-            iconURL: "https://devicons.railway.app/i/railway-light.svg",
-        })
-        .setTimestamp();
+            .setAuthor({
+                name: "Deployment info:",
+            })
+            .setTitle(projectName)
+            .setDescription(`**${status}**`)
+            .addFields(
+                {
+                    name: "Enviroment",
+                    value: environmentName,
+                    inline: true
+                },
+                {
+                    name: "Service",
+                    value: serviceName,
+                    inline: true
+                },
+                {
+                    name: "Commit Author",
+                    value: deploymentCommitAuthor,
+                    inline: true
+                },
+                {
+                    name: "Commit Message",
+                    value: deploymentCommit,
+                    inline: false
+                },
+            )
+            .setColor("#00ff40")
+            .setFooter({
+                text: "RailwayBot",
+                iconURL: "https://devicons.railway.app/i/railway-light.svg",
+            })
+            .setTimestamp();
+    }
+    else {
+        embed = new EmbedBuilder()
+            .setAuthor({
+                name: "Deployment Crashed:",
+            })
+            .setTitle(projectName)
+            .addFields(
+                {
+                    name: "Enviroment",
+                    value: environmentName,
+                    inline: true
+                },
+                {
+                    name: "Service",
+                    value: serviceName,
+                    inline: true
+                },
+                {
+                    name: "Commit Author",
+                    value: deploymentCommitAuthor,
+                    inline: true
+                },
+                {
+                    name: "Commit Message",
+                    value: deploymentCommit,
+                    inline: false
+                },
+            )
+            .setColor("#ff0000")
+            .setFooter({
+                text: "RailwayBot",
+                iconURL: "https://devicons.railway.app/i/railway-light.svg",
+            })
+            .setTimestamp();
     }
     channel.send({ embeds: [embed] })
 }
@@ -174,4 +282,68 @@ interface DeploymentMeta {
     branch: string;
     commitAuthor: string;
     commitMessage: string;
+}
+//Usage type
+interface Project {
+    name: string;
+    id: string;
+    CpuUsage: number;
+    MemUsage: number;
+    Egress: number;
+    EstimatedCpuUsage: number;
+    EstimatedMemUsage: number;
+    EstimatedEgress: number;
+}
+//API response type
+interface GraphQLResponse {
+    usage: Usage[]
+    estimatedUsage: EstimatedUsage[]
+    me: Me
+}
+
+interface Usage {
+    value: number
+    measurement: string
+    tags: Tags
+}
+
+interface Tags {
+    projectId: string
+}
+
+interface EstimatedUsage {
+    estimatedValue: number
+    measurement: string
+    projectId: string
+}
+
+interface Me {
+    projects: Projects
+}
+
+interface Projects {
+    edges: Edge[]
+}
+
+interface Edge {
+    node: Node
+}
+
+interface Node {
+    id: string
+    name: string
+}
+
+// General error handling
+process.on("uncaughtException", function (err) {
+    console.log(`Fatal error occured:`);
+    console.error(err);
+    if (client !== undefined) {
+        logError(err);
+    }
+});
+
+async function logError(err: any) {
+    const logChannel = await client.channels.fetch(`${process.env.LOGCHANNEL}`) as TextChannel
+    logChannel.send({ content: `**Fatal error experienced:** ${err}` });
 }
